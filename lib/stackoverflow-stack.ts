ed0a4af4f74bb16c4e85913c073287834d16e24a
Paul Mustacescu <paulmustacescu@Personal.local>
2024-05-22 23:02:25 +0300

    added auth

--- lib/stackoverflow-stack.ts
+++ lib/stackoverflow-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import {
   AccessLogField,
   AccessLogFormat,
+  AuthorizationType,
+  CognitoUserPoolsAuthorizer,
   Cors,
   Deployment,
   LambdaIntegration,
@@ -9,6 +11,15 @@ import {
   RestApi,
   Stage,
 } from "aws-cdk-lib/aws-apigateway";
+import {
+  OAuthScope,
+  ProviderAttribute,
+  ResourceServerScope,
+  UserPool,
+  UserPoolClient,
+  UserPoolClientIdentityProvider,
+  UserPoolIdentityProviderGoogle,
+} from "aws-cdk-lib/aws-cognito";
 import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Architecture, Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
@@ -20,6 +31,47 @@ import path = require("path");
 export class StackoverflowStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const userPool = new UserPool(this, "StackOverflowUserPool");
+
+    const userPoolDomain = userPool.addDomain("StackOverflowDomainName", {
+      cognitoDomain: {
+        domainPrefix: `stackoverflow-light-xyz-auth-api`,
+      },
+    });
+
+    const appScope = new ResourceServerScope({
+      scopeName: "app",
+      scopeDescription: "App scope for StackOverflow App",
+    });
+
+    const resourceServer = userPool.addResourceServer("ResourceServer", {
+      userPoolResourceServerName: "stack-overflow-resource-server",
+      identifier: "stack-overflow-resource-server",
+      scopes: [appScope],
+    });
+
+    const oauthAppScope = OAuthScope.resourceServer(resourceServer, appScope);
+
+    const poolClient = userPool.addClient("stack-overflow-app-client", {
+      userPoolClientName: "StackOverflowAppClient",
+      idTokenValidity: cdk.Duration.days(1),
+      accessTokenValidity: cdk.Duration.days(1),
+      authFlows: {
+        userPassword: false,
+        userSrp: false,
+        custom: true,
+      },
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: false,
+          implicitCodeGrant: false,
+          clientCredentials: true,
+        },
+        scopes: [oauthAppScope],
+      },
+      preventUserExistenceErrors: true,
+      generateSecret: true,
+    });
 
     const table = new Table(this, "StackOverflowTable", {
       partitionKey: { name: "PK", type: AttributeType.STRING },
@@ -93,6 +145,14 @@ export class StackoverflowStack extends cdk.Stack {
       architecture: Architecture.ARM_64,
     });
 
+    const cognitoAuthoriser = new CognitoUserPoolsAuthorizer(
+      this,
+      "CognitoAuthorizer",
+      {
+        cognitoUserPools: [userPool],
+      },
+    );
+
     const stackOverflowApi = new RestApi(this, "StackOverflowApi", {
       description: "The StacOverflow API",
       defaultCorsPreflightOptions: {
@@ -115,17 +175,76 @@ export class StackoverflowStack extends cdk.Stack {
     const votesPath = stackOverflowApi.root.addResource("votes");
     const analyticsPath = stackOverflowApi.root.addResource("analytics");
 
-    questionsPath.addMethod("POST", new LambdaIntegration(createQuestion));
-    questionsPath.addMethod("GET", new LambdaIntegration(getQuestions));
-    questionPath.addMethod("GET", new LambdaIntegration(getQuestion));
-    answersPath.addMethod("POST", new LambdaIntegration(createAnswer));
-    votesPath.addMethod("POST", new LambdaIntegration(createVotes));
-    analyticsPath.addMethod("GET", new LambdaIntegration(getAnalytics));
+    questionsPath.addMethod("POST", new LambdaIntegration(createQuestion), {
+      authorizer: cognitoAuthoriser,
+      authorizationType: AuthorizationType.COGNITO,
+      authorizationScopes: [oauthAppScope.scopeName],
+    });
+    questionsPath.addMethod("GET", new LambdaIntegration(getQuestions), {
+      authorizer: cognitoAuthoriser,
+      authorizationType: AuthorizationType.COGNITO,
+      authorizationScopes: [oauthAppScope.scopeName],
+    });
+    questionPath.addMethod("GET", new LambdaIntegration(getQuestion), {
+      authorizer: cognitoAuthoriser,
+      authorizationType: AuthorizationType.COGNITO,
+      authorizationScopes: [oauthAppScope.scopeName],
+    });
+    answersPath.addMethod("POST", new LambdaIntegration(createAnswer), {
+      authorizer: cognitoAuthoriser,
+      authorizationType: AuthorizationType.COGNITO,
+      authorizationScopes: [oauthAppScope.scopeName],
+    });
+    votesPath.addMethod("POST", new LambdaIntegration(createVotes), {
+      authorizer: cognitoAuthoriser,
+      authorizationType: AuthorizationType.COGNITO,
+      authorizationScopes: [oauthAppScope.scopeName],
+    });
+    analyticsPath.addMethod("GET", new LambdaIntegration(getAnalytics), {
+      authorizer: cognitoAuthoriser,
+      authorizationType: AuthorizationType.COGNITO,
+      authorizationScopes: [oauthAppScope.scopeName],
+    });
 
     const deployment = new Deployment(this, "stackOverflowApiDeployment", {
       api: stackOverflowApi,
     });
 
-    new Stage(this, `v1`, { deployment, stageName: "v1" });
+    const v1Stage = new Stage(this, `v1`, { deployment, stageName: "v1" });
+
+    stackOverflowApi.deploymentStage = v1Stage;
+    // const userPool = new UserPool(this, "StackOverflowUserPool");
+    //
+    // const userPoolDomain = userPool.addDomain("StackOverflowDomainName", {
+    //   cognitoDomain: {
+    //     domainPrefix: `stackoverflow-light-xyz-auth-api`,
+    //   },
+    // });
+    //
+    // new UserPoolIdentityProviderGoogle(this, "Google", {
+    //   userPool,
+    //   clientId:"",
+    //   clientSecret: "",
+    //
+    //   // Email scope is required, because the default is 'profile' and that doesn't allow Cognito
+    //   // to fetch the user's email from his Google account after the user does an SSO with Google
+    //   scopes: ["email"],
+    //
+    //   // Map fields from the user's Google profile to Cognito user fields, when the user is auto-provisioned
+    //   attributeMapping: {
+    //     email: ProviderAttribute.GOOGLE_EMAIL,
+    //   },
+    // });
+    //
+    // const callbackUrl = "...";
+    //
+    // const client = new UserPoolClient(this, "UserPoolClient", {
+    //   userPool,
+    //   generateSecret: true,
+    //   supportedIdentityProviders: [UserPoolClientIdentityProvider.GOOGLE],
+    //   oAuth: {
+    //     callbackUrls: [callbackUrl],
+    //   },
+    // });
   }
 }
